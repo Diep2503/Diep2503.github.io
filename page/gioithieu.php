f71a53944d575a34756913a34fac16fd81b35545
Diep2503 <bongiagia01@gmail.com>
2021-10-18 20:51:28 +0700

    huhu

--- page/gioithieu.php
+++
@@ -1,9 +0,0 @@
-<div class="gioithieu_gt">
-            <h3>GIỚI THIỆU</h3>
-        </div>
-        <div class="gioithieu_gt_text">
-            <p class="td">HAVE A CUP AND LEVEL UP</p>
-            <p>Mỗi ngày, The Dducky Cafe có cơ hội được phục vụ và mang đến những thức uống chỉn chu đến với khách hàng mà còn mang đến không gian trải nghiệm đầy cảm hứng và khác biệt.</p>
-            <p>Sứ mệnh của chúng tôi là cung cấp những giá trí tích cực từ đồ ăn, thức uống đến không gian giúp cho từng vị khách của mình "tăng hạng" những ý tưởng, suy nghĩ trong cuộc sống. Một nơi để trao đổi, gặp gỡ và cùng chia sẻ những câu chuyện </p>
-            <p>The Dducky Cafe sẽ luôn cố gắng hoàn thiện và tạo dựng một môi trường tốt hơn những gì mà chúng tôi đã làm.</p>
-        </div>
